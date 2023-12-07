173460aec606c870378ea6e131691c65bd898166
oni <oni@cynetlab.com>
2023-12-08 00:39:38 +0900

    change

--- serial-agent-wifi/scripts/client.py
+++ serial-agent-wifi/scripts/client.py
@@ -15,17 +15,23 @@ def main(jsonfile):
     with open(jsonfile) as fp:
         conf = json.load(fp)
     ip = conf['ip']
-    port = conf['port'] if 'port' in conf else 80
+    port = int(conf['port']) if 'port' in conf else 80
+    nl = conf['nl'] if 'nl' in conf else "NL"
     server = (ip, port)
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         set_nonblocking(sys.stdin)
-        sock.settimeout(1)
         sock.connect(server)
         sock.setblocking(False)
         while True:
             conin = sys.stdin.readline()
             if conin != "":
+                if nl == "CR":
+                    # 改行コードを置換する nl -> cr
+                    conin = conin.replace('\n', '\r')
+                elif nl == "CRNL":
+                    # 改行コードを置換する nl -> crnl
+                    conin = conin.replace('\n', '\r\n')
                 sock.sendall(bytes((conin).encode("utf-8")))
                 if conin.startswith('cnl:quit'):
                     break
